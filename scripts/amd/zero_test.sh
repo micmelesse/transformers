8f19127233c8eecce719f1ebce75432e688ae30f
Michael Melesse <micmelesse@gmail.com>
2021-01-25 20:38:21 -0500

    log loss every step

--- scripts/amd/zero_test.sh
+++ scripts/amd/zero_test.sh
@@ -17,7 +17,7 @@ echo "baseline"
 PYTHONPATH=../../src USE_TF=0 python -m torch.distributed.launch \
     --nproc_per_node=2 ./finetune_trainer.py --model_name_or_path t5-large --output_dir output_dir \
     --adam_eps 1e-06 --data_dir wmt_en_ro --do_eval --do_train --evaluation_strategy=steps --freeze_embeds \
-    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1000 --max_source_length 128 \
+    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1 --max_source_length 128 \
     --max_target_length 128 --num_train_epochs 1 --overwrite_output_dir --per_device_eval_batch_size $BS \
     --per_device_train_batch_size $BS --predict_with_generate --eval_steps 25000 --sortish_sampler \
     --task translation_en_to_ro --test_max_target_length 128 --val_max_target_length 128 --warmup_steps 500 \
@@ -29,7 +29,7 @@ echo "w/ --fp16"
 PYTHONPATH=../../src USE_TF=0 python -m torch.distributed.launch \
     --nproc_per_node=2 ./finetune_trainer.py --model_name_or_path t5-large --output_dir output_dir \
     --adam_eps 1e-06 --data_dir wmt_en_ro --do_eval --do_train --evaluation_strategy=steps --freeze_embeds \
-    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1000 --max_source_length 128 \
+    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1 --max_source_length 128 \
     --max_target_length 128 --num_train_epochs 1 --overwrite_output_dir --per_device_eval_batch_size $BS \
     --per_device_train_batch_size $BS --predict_with_generate --eval_steps 25000 --sortish_sampler \
     --task translation_en_to_ro --test_max_target_length 128 --val_max_target_length 128 --warmup_steps 500 \
@@ -41,7 +41,7 @@ echo "w/ --sharded_ddp"
 PYTHONPATH=../../src USE_TF=0 python -m torch.distributed.launch \
     --nproc_per_node=2 ./finetune_trainer.py --model_name_or_path t5-large --output_dir output_dir \
     --adam_eps 1e-06 --data_dir wmt_en_ro --do_eval --do_train --evaluation_strategy=steps --freeze_embeds \
-    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1000 --max_source_length 128 \
+    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1 --max_source_length 128 \
     --max_target_length 128 --num_train_epochs 1 --overwrite_output_dir --per_device_eval_batch_size $BS \
     --per_device_train_batch_size $BS --predict_with_generate --eval_steps 25000 --sortish_sampler \
     --task translation_en_to_ro --test_max_target_length 128 --val_max_target_length 128 --warmup_steps 500 \
@@ -53,7 +53,7 @@ echo "w/ --sharded_ddp --fp16"
 PYTHONPATH=../../src USE_TF=0 python -m torch.distributed.launch \
     --nproc_per_node=2 ./finetune_trainer.py --model_name_or_path t5-large --output_dir output_dir \
     --adam_eps 1e-06 --data_dir wmt_en_ro --do_eval --do_train --evaluation_strategy=steps --freeze_embeds \
-    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1000 --max_source_length 128 \
+    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1 --max_source_length 128 \
     --max_target_length 128 --num_train_epochs 1 --overwrite_output_dir --per_device_eval_batch_size $BS \
     --per_device_train_batch_size $BS --predict_with_generate --eval_steps 25000 --sortish_sampler \
     --task translation_en_to_ro --test_max_target_length 128 --val_max_target_length 128 --warmup_steps 500 \
@@ -65,7 +65,7 @@ echo "w/ --deepspeed ds_config.json (stage 2 w/o cpu offloading)"
 PYTHONPATH=../../src USE_TF=0 python -m torch.distributed.launch \
     --nproc_per_node=2 ./finetune_trainer.py --model_name_or_path t5-large --output_dir output_dir \
     --adam_eps 1e-06 --data_dir wmt_en_ro --do_eval --do_train --evaluation_strategy=steps --freeze_embeds \
-    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1000 --max_source_length 128 \
+    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1 --max_source_length 128 \
     --max_target_length 128 --num_train_epochs 1 --overwrite_output_dir --per_device_eval_batch_size $BS \
     --per_device_train_batch_size $BS --predict_with_generate --eval_steps 25000 --sortish_sampler \
     --task translation_en_to_ro --test_max_target_length 128 --val_max_target_length 128 --warmup_steps 500 \
@@ -77,7 +77,7 @@ echo "w/ --deepspeed ds_config.json (stage 2 w/ cpu offloading)"
 PYTHONPATH=../../src USE_TF=0 python -m torch.distributed.launch \
     --nproc_per_node=2 ./finetune_trainer.py --model_name_or_path t5-large --output_dir output_dir \
     --adam_eps 1e-06 --data_dir wmt_en_ro --do_eval --do_train --evaluation_strategy=steps --freeze_embeds \
-    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1000 --max_source_length 128 \
+    --label_smoothing 0.1 --learning_rate 3e-5 --logging_first_step --logging_steps 1 --max_source_length 128 \
     --max_target_length 128 --num_train_epochs 1 --overwrite_output_dir --per_device_eval_batch_size $BS \
     --per_device_train_batch_size $BS --predict_with_generate --eval_steps 25000 --sortish_sampler \
     --task translation_en_to_ro --test_max_target_length 128 --val_max_target_length 128 --warmup_steps 500 \
