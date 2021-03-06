425410e7843a263d9b8e0ae3875e4666948f76ab
Michael Melesse <micmelesse@gmail.com>
2021-03-05 20:51:32 -0500

    add manual seed

--- src/transformers/models/t5/modeling_t5.py
+++ src/transformers/models/t5/modeling_t5.py
@@ -924,6 +924,7 @@ class T5Stack(T5PreTrainedModel):
         encoder_decoder_position_bias = None
 
         save_tensor(inputs_embeds, "modeling_t5:T5ForConditionalGeneration:T5Stack:inputs_embeds_before_layers_before_dropout")
+        torch.manual_seed(1234)
         hidden_states = self.dropout(inputs_embeds)
         save_tensor(hidden_states, "modeling_t5:T5ForConditionalGeneration:T5Stack:hidden_states_before_layers_after_dropout")
 
