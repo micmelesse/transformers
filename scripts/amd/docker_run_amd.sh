4fef034d6c3f0db5688c9a7e84f56cd048ce239c
Michael Melesse <micmelesse@gmail.com>
2021-03-10 20:05:27 -0500

    add dockerfile

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -1,8 +1,17 @@
-alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx -v /data:/data'
+alias drun='sudo docker run -it --rm --network=host --device=/dev/kfd --device=/dev/dri --ipc=host --shm-size 16G --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined'
 
-WORK_DIR='/dockerx/transformers'
+VOLUMES="-v $HOME/dockerx:/dockerx"
+# WORK_DIR='-w /root/transformers'
 
-# drun -w $WORK_DIR rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch_1.7.0_apex_c1e88fa
-# drun -w $WORK_DIR rocm/tensorflow:rocm4.0.1-tf2.3-dev
-drun -w $WORK_DIR rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
-# drun -w $WORK_DIR rocm/pytorch-private:rocm4.1rel_ub18_rocm_41_internal_pytorch_bnorm_patch_SSD_updated
+
+# IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch_1.7.0_apex_c1e88fa
+# IMAGE_NAME=rocm/tensorflow:rocm4.0.1-tf2.3-dev
+# IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
+# IMAGE_NAME=rocm/pytorch-private:rocm4.1rel_ub18_rocm_41_internal_pytorch_bnorm_patch_SSD_updated
+IMAGE_NAME=zero
+
+CONTAINER_NAME=${IMAGE_NAME}_container
+
+drun -d --name $CONTAINER_NAME $WORK_DIR $VOLUMES $IMAGE_NAME
+# docker cp scripts/amd $CONTAINER_NAME:/root/transformers/scripts
+docker attach $CONTAINER_NAME
---
+++ scripts/amd/zero.Dockerfile
@@ -0,0 +1,27 @@
+# Select base Image
+FROM rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
+
+# Install dependencies
+RUN apt update && apt install -y \
+    unzip 
+RUN pip3 install regex sacremoses filelock gitpython rouge_score sacrebleu deepspeed fairscale
+
+# install transformers
+RUN cd /root &&\
+    git clone https://github.com/huggingface/transformers &&\
+    cd transformers &&\
+    git checkout 96897a353564e45141480a0260ad1314716bace7 &&\
+    pip install -e .
+
+# set work dir
+WORKDIR /root/transformers
+
+# Download data
+RUN cd examples/seq2seq &&\
+    wget https://cdn-datasets.huggingface.co/translation/wmt_en_ro.tar.gz &&\
+    tar -xzvf wmt_en_ro.tar.gz 
+
+
+
+
+
