25a26d8e717c7b3730744a4e1bdd1991ab9a0ac7
Michael Melesse <micmelesse@gmail.com>
2021-03-12 17:41:38 -0500

    update test dropout

--- test_dropout/dropout_test.py
+++ test_dropout/dropout_test.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 from torch.nn.parameter import Parameter
 import torch.nn.functional as F
+import numpy as np
+import random
 
 # load tracer util function
 import importlib.util
@@ -11,7 +13,14 @@ spec.loader.exec_module(tracer)
 
 # stablize random elements
 torch.set_deterministic(True)
-torch.manual_seed(1234)
+seed=42
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)  
+np.random.seed(seed)  
+random.seed(seed)  
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
 
 # load input tensors
 attn_weights = torch.load("modeling_t5:T5ForConditionalGeneration:T5Stack:T5Block:T5LayerSelfAttention:T5Attention:attn_weights_before_dropout_0.pt")
@@ -23,4 +32,5 @@ output = F.dropout(
 )
 
 # save output
+tracer.init_hostdir()
 tracer.save_tensor(output, 'dropout_output')
