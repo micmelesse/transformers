4370fb09ecff66cf13ceb1e8c76a320e1e3f5fbc
Michael Melesse <micmelesse@gmail.com>
2021-03-12 15:01:19 -0500

    add dropout test

---
+++ test_dropout/dropout_test.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+from torch.nn.parameter import Parameter
+import torch.nn.functional as F
+
+torch.set_deterministic(True)
+torch.manual_seed(1234)
+
+attn_weights = torch.load("modeling_t5:T5ForConditionalGeneration:T5Stack:T5Block:T5LayerSelfAttention:T5Attention:attn_weights_before_dropout_0.pt")
+attn_weights = attn_weights.half().cuda()
+
+output = F.dropout(
+    attn_weights, p=0.1, training=True
+)
+
+torch.save(output, 'dropout_output.pt')
