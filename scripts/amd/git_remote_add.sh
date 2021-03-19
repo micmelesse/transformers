16d6f3ff19ab49b99019fecf39570a4475eb1e88
Michael Melesse <micmelesse@gmail.com>
2021-03-18 19:57:59 -0500

    add software

---
+++ scripts/amd/git_remote_add.sh
@@ -0,0 +1,3 @@
+git remote add upstream https://github.com/huggingface/transformers
+git remote add rocm_fork https://github.com/ROCmSoftwarePlatform/transformers
+git remote add private_fork https://github.com/micmelesse/transformers
